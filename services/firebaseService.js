c483bb6667ee63eb5c16101fb864c2b2d146ffe3
Chaitanya1024 <jainchaitanya1024@gmail.com>
2024-07-22 16:04:19 +0530

    added firebase credential to base 64

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -1,10 +1,13 @@
 const admin = require('firebase-admin');
-require('web-streams-polyfill/polyfill');
+const { Base64 } = require('js-base64'); // Add this dependency if needed
+
+// Decode the base64-encoded service account key
+const serviceAccountKeyBase64 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const serviceAccountKey = JSON.parse(Base64.decode(serviceAccountKeyBase64));
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('../mern-igm-b453e942aa81.json');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountKey),
     storageBucket: `${process.env.PROJECT_ID}.appspot.com`
 });
 
